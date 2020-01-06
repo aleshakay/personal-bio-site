cf3be3f09e261347098446ad519b09d66f01cb96
alesha reed <aleshak83@gmail.com>
2020-01-06 13:55:31 -0600

    added hover

--- src/javascripts/Components/ProjectFormat/ProjectFormat.js
+++ src/javascripts/Components/ProjectFormat/ProjectFormat.js
@@ -4,12 +4,13 @@ const createProjectCards = (project) => {
   let domString = '';
   domString += `
 <div class="container-fluid ">
-        <div class="row">
+        <div class="box">
             <div class="card projCard">
-            <div class "col-4">
                 <h2>${project.title}</h2>
                 <h3>${project.description}</h3>
-                <img src=${project.screenshot} alt='Image of ${project.title}' />
+                <div class="imgbx">
+                <img src=${project.screenshot}"auto=format&fit=crop&w=500&q=60" alt='Image of ${project.title}' />
+                </div>
                 <h3>${project.technologiesUsed}</h3>
                 <h3 class="projectURL"><a href=${project.url}>url</a></h3>
                 <h3 class="gitURL"><a href=${project.githubUrl}>GitHub</a></h3>
